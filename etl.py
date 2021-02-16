6d52669a852af70abdbbe74bc1675a837420d84b
Dmitry Chirikov <dmitry.chirikov@brightcomputing.com>
2021-02-16 23:50:11 +0100

    Process song data

--- etl.py
+++ etl.py
@@ -23,22 +23,24 @@ def create_spark_session():
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
-    song_data = 
-    
+    song_data = f"{input_data}/song_data/*/*/*"
+
     # read song data file
-    df = 
+    df = spark.read.json(song_data)
 
     # extract columns to create songs table
-    songs_table = 
-    
+    song_cols = ["song_id", "title", "artist_id", "year", "duration"]
+    songs_table = df.select(*song_cols)
+
     # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table.write.partitionBy("year","artist_id").parquet(f"{output_data}/songs")
 
     # extract columns to create artists table
-    artists_table = 
-    
+    artist_cols = ["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude"]
+    artists_table = df.select(*artist_cols)
+
     # write artists table to parquet files
-    artists_table
+    artists_table.write.parquet(f"{output_data}/artists")
 
 
 def process_log_data(spark, input_data, output_data):
@@ -46,36 +48,36 @@ def process_log_data(spark, input_data, output_data):
     log_data =
 
     # read log data file
-    df = 
-    
-    # filter by actions for song plays
-    df = 
+    df =
 
-    # extract columns for users table    
-    artists_table = 
-    
+    # filter by actions for song plays
+    df =
+
+    # extract columns for users table
+    artists_table =
+
     # write users table to parquet files
     artists_table
 
     # create timestamp column from original timestamp column
     get_timestamp = udf()
-    df = 
-    
+    df =
+
     # create datetime column from original timestamp column
     get_datetime = udf()
-    df = 
-    
+    df =
+
     # extract columns to create time table
-    time_table = 
-    
+    time_table =
+
     # write time table to parquet files partitioned by year and month
     time_table
 
     # read in song data to use for songplays table
-    song_df = 
+    song_df =
 
-    # extract columns from joined song and log datasets to create songplays table 
-    songplays_table = 
+    # extract columns from joined song and log datasets to create songplays table
+    songplays_table =
 
     # write songplays table to parquet files partitioned by year and month
     songplays_table
@@ -85,8 +87,8 @@ def main():
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
     output_data = ""
-    
-    process_song_data(spark, input_data, output_data)    
+
+    process_song_data(spark, input_data, output_data)
     process_log_data(spark, input_data, output_data)
 
 
